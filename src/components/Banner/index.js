46a7a2cceef825c7beecd17b1a5b1e9bff22f42e
DevRuiz18 <gustavosruiz@gmail.com>
2023-01-22 15:20:52 -0300

    banner done

---
+++ src/components/Banner/index.js
@@ -0,0 +1,34 @@
+import styles from "./Banner.module.css";
+import circuloColorido from 'assets/video-2.3/assets/circulo_colorido.png'
+
+export default function Banner() {
+  return (
+    <div className={styles.banner}>
+      <div className={styles.apresentacao}>
+        <h1 className={styles.titulo}>Olá, mundo</h1>
+
+        <p className={styles.paragrafo}>
+          Boas vindas ao meu espaço pessoal! Eu sou Gustavo Ruiz, instrutor de
+          Front-end da Alura. Aqui compartilho vários conhecimentos, espero que
+          aprenda algo novo :)
+        </p>
+      </div>
+
+      <div className={styles.imagens}>
+        <img 
+            className={styles.circuloColorido} 
+            src={circuloColorido} 
+            aria-hiden={true}
+            alt="meu deus q ngc insuportavel"    
+        ></img>
+        <img 
+            className={styles.minhaFoto} 
+            src="https://github.com/gussruiz.png"
+            alt="foto minha sorrindo"
+        ></img>
+      </div>
+
+
+    </div>
+  );
+}
